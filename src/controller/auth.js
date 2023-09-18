eeaee1f9237c31869ef193b742460f8b66e1d220
Gautam-121 <gautamdhakate1234@gmail.com>
2023-09-18 12:09:14 +0530

    auth done

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -8,8 +8,14 @@ const auth = async(req , res)=>{
 
 const redirection = async(req , res)=>{
     const data = (await redirects(req.query.code , req.query.shop))
-    console.log(data)
-    res.json(data)
+
+    process.env.shopify_token = data.access_token
+    process.env.shop = req.query.shop
+
+    return res.json({
+        success : true,
+        message : "Authorization Done!"
+    })
 }
 
 module.exports = {auth , redirection}
--- src/utils/shopifyAuthHelper.js
+++ src/utils/shopifyAuthHelper.js
@@ -2,7 +2,7 @@ const axios = require("axios")
 
 const authorize = async(shop)=>{
     console.log("Enter")
-    return encodeURI(`https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${process.env.client_id}&scope=${process.env.scope}&redirect_uri=${process.env.redirect_uri}`)
+    return encodeURI(`https://${shop}/admin/oauth/authorize?client_id=${process.env.client_id}&scope=${process.env.scope}&redirect_uri=${process.env.redirect_uri}`)
 }
 
 const redirects = async(code , shop)=>{
